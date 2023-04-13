357f8794f4ba25b84464165c46c0a3e80c0a1138
supreme_kob <jakob.bellamy@supremelending.com>
2023-04-13 12:05:19 -0400

    Initial Commit

---
+++ Saute-App/Saute_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Saute_AppApp.swift
+//  Saute-App
+//
+//  Created by Jakob Bellamy on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Saute_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
